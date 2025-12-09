590d57be22a08efe5dbe1a4613b743ddca733e06
Athallah <162315317+KoharuKoval@users.noreply.github.com>
2025-12-09 19:51:34 +0700

    Update Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -14,3 +14,32 @@ public class Penumpang {
         this.hamil = hamil;
         this.saldo = 10000; 
     }
+
+public int getID() { return id; }
+    public String getNama() { return nama; }
+    public int getUmur() { return umur; }
+    public boolean isHamil() { return hamil; } 
+    public int getSaldo() { return saldo; }
+
+    public boolean isPrioritas() {
+        return umur > 60 || umur < 10 || hamil; 
+    }
+
+    public void tambahSaldo(int saldobaru) {
+        this.saldo += saldobaru; 
+    }
+
+    public void kurangiSaldo(int ongkos) {
+        if (this.saldo >= ongkos) {
+            this.saldo -= ongkos;
+        } 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Penumpang penumpang = (Penumpang) o;
+        return Objects.equals(nama, penumpang.nama);
+    }
+}
